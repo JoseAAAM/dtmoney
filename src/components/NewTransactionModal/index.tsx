a9ea9e91f183b5d963e1a6ff05eb5bf063edf0fc
José Augusto <guto.machoski@gmail.com>
2021-11-16 23:12:15 -0300

    Static screens

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+import Modal from "react-modal";
+
+import { Container, TransactionTypeContainer, RadialBox } from "./styles";
+
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <RadialBox
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadialBox>
+          <RadialBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Entrada</span>
+          </RadialBox>
+        </TransactionTypeContainer>
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
